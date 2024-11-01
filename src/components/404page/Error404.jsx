b4d70d029dfa095172edeb9866bb3c5975b3e0ee
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2024-11-01 11:55:47 +0530

    404 page fix

--- src/components/404page/Error404.jsx
+++ src/components/404page/Error404.jsx
@@ -3,7 +3,7 @@ import error404 from "../../../public/error404.jpg";
 const Error404 = () => {
   return (
     <div className="w-full h-screen flex flex-col gap-4 items-center justify-center bg-white">
-      <div className="w-[40rem] bg-red-300">
+      <div className="w-[40rem] h-auto">
         <img src={error404} alt="404 page not found" />
       </div>
       <h1 className="text-center text-2xl uppercase font-sans font-semibold">
